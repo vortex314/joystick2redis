b20eb6e684c1ecc9e7b76e102db7b5cbf43693cd
lieven <lieven.merckx@gmail.com>
2022-07-14 01:18:10 +0200

    config cleanup

--- src/joystick2redis.cpp
+++ src/joystick2redis.cpp
@@ -1,4 +1,5 @@
 #include <Config.h>
+#include <ConfigFile.h>
 #include <Hardware.h>
 #include <Joystick.h>
 #include <Log.h>
@@ -13,9 +14,11 @@ std::string loadFile(const char *name);
 bool loadConfig(JsonObject cfg, int argc, char **argv);
 
 int main(int argc, char **argv) {
-  INFO("Start %s", argv[0]);
-  DynamicJsonDocument config(10240);
-  loadConfig(config.to<JsonObject>(), argc, argv);
+  INFO("Loading configuration.");
+  Json config;
+  config["redis"]["host"] = "localhost";
+  config["redis"]["port"] = 6379;
+  configurator(config, argc, argv);
   Thread workerThread("worker");
   Joystick joystick(workerThread);  // blocking thread
   Redis redis(workerThread, config["redis"].as<JsonObject>());
@@ -40,63 +43,7 @@ int main(int argc, char **argv) {
   };
 
   redis.response() >> [&](const Json &) {
-//ignore responses from redis
+    // ignore responses from redis
   };
   workerThread.run();
 }
-
-void deepMerge(JsonVariant dst, JsonVariant src)
-{
-    if (src.is<JsonObject>())
-    {
-        for (auto kvp : src.as<JsonObject>())
-        {
-            deepMerge(dst.getOrAddMember(kvp.key()), kvp.value());
-        }
-    }
-    else
-    {
-        dst.set(src);
-    }
-}
-
-bool loadConfig(JsonObject cfg, int argc, char **argv)
-{
-    // override args
-    int c;
-    while ((c = getopt(argc, argv, "h:p:s:b:f:t:v")) != -1)
-        switch (c)
-        {
-        case 'f':
-        {
-            std::string s = loadFile(optarg);
-            DynamicJsonDocument doc(10240);
-            deserializeJson(doc, s);
-            deepMerge(cfg, doc);
-            break;
-        }
-        case 'h':
-            cfg["redis"]["host"] = optarg;
-            break;
-        case 'p':
-            cfg["redis"]["port"] = atoi(optarg);
-            break;
-        case 'v':
-        {
-            logger.setLevel(Log::L_DEBUG);
-            break;
-        }
-        case '?':
-            printf("Usage %s -v -f <configFile.json>",
-                   argv[0]);
-            break;
-        default:
-            WARN("Usage %s -v -f <configFile.json> ",
-                 argv[0]);
-            abort();
-        }
-    std::string s;
-    serializeJson(cfg, s);
-    INFO("config:%s", s.c_str());
-    return true;
-};
